7e63558bf0cbc0a4b949432925a78a157e96f35d
Yoshiki Honda <yoshonda@yahoo-corp.jp>
2019-10-05 22:48:13 +0900

    third commit

---
+++ src/tests/components/Counter.test.tsx
@@ -0,0 +1,13 @@
+import { shallow } from 'enzyme'
+import Counter from '../../components/counter/Counter'
+
+describe('Counter', () => {
+  it('should render the text `Count: <count>` for the given count prop', () => {
+    const counter_actions = {
+      increment: jest.fn(),
+      remove: jest.fn()
+    }
+    const component = shallow(<Counter count={1} counter_actions={counter_actions} />)
+    expect(component.text()).toContain('Count: 1')
+  })
+})
